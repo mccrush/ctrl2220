a49af80ee9d26c99eaeac1f42603b93627f49353
mccrush <mccrush2027@gmail.com>
2020-05-21 13:06:38 +0700

    Edit App component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,65 @@
 <template>
-  <div>
-    <Navbar />
-    <router-view />
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <Navbar />
+    </transition>
+
+    <div class="container mt-4 border">
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
+    </div>
+
+    <transition name="fade" mode="out-in">
+      <Footer />
+    </transition>
   </div>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
 <style>
+#app {
+  font-family: "Exo 2", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background: #f2f2f2;
+}
+
+.container {
+  max-width: 1130px;
+  background: #fff;
+  padding-bottom: 24px;
+}
+
+.fw700 {
+  font-weight: 700;
+}
+
+.fw400 {
+  font-weight: 400;
+}
+
+a {
+  color: #0056b3;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
 </style>
---
+++ src/views/HotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row justify-content-center">
+    <vueHeadful title="Ошибка 404" description="Запрашиваемая страница не найдена" />
+    <div class="col-12 col-sm-8 col-md-6 col-xl-6">
+      <h5>Ошибка 404</h5>
+      <hr />
+      <p>Запрашиваемая страница не найдена</p>
+      <router-link to="/" class="btn btn-block btn-light border">Вернуться на главную страницу</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import vueHeadful from "vue-headful";
+export default {
+  name: "notfound",
+  components: {
+    vueHeadful
+  }
+};
+</script>
