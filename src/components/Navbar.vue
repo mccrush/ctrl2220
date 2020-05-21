d4631a6e186f80213e31a2016833d0dea7cf3b94
mccrush <mccrush2027@gmail.com>
2020-05-21 13:13:59 +0700

    Edit Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,30 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-sm navbar-light bg-white shadow-sm">
+    <router-link to="/" class="navbar-brand">Ctrl22</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link to="/reshen" class="nav-link">Reshen</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/naprav" class="nav-link">Naprav</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/rtsp" class="nav-link">RTSP</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/admin" class="nav-link">-=Admin=-</router-link>
+        </li>
+      </ul>
     </div>
-  </div>
+  </nav>
 </template>
+
+
