9e080c71cd32b140838644c1940776b66bd994b5
mccrush <mccrush2027@gmail.com>
2020-05-21 12:02:17 +0700

    Add config file

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  productionSourceMap: false,
+
+  pwa: {
+    name: 'Полный контроль',
+    themeColor: '#FF0000',
+    msTileColor: '#FFFFFF',
+    manifestOptions: {
+      background_color: '#FF0000'
+    }
+  }
+}
