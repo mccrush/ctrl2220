060453f28e101275f975e50dd0c5d9bb257087df
mccrush <mccrush2027@gmail.com>
2020-05-21 12:28:48 +0700

    Add firebase config

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import { apiKey } from '../apiKey'
+const firebaseConfig = {
+  apiKey,
+  authDomain: "ctrl2220.firebaseapp.com",
+  databaseURL: "https://ctrl2220.firebaseio.com",
+  projectId: "ctrl2220",
+  storageBucket: "ctrl2220.appspot.com",
+  messagingSenderId: "31311777944",
+  appId: "1:31311777944:web:09ea4ea8bdb2ae3a2d80ed",
+  measurementId: "G-3566KJWVFP"
+};
+
 new Vue({
   router,
   store,
