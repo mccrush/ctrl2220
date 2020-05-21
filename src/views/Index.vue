e51910b4d0f68d61eda3f765dae2c5021871ca46
mccrush <mccrush2027@gmail.com>
2020-05-21 12:49:24 +0700

    Create app struction

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "index",
+  components: {
+    Navbar
+  }
+};
+</script>
