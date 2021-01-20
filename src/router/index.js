4e3f9ce46e18670ed02e958302eab03fe2bf9dbf
mccrush <mccrush2027@gmail.com>
2021-01-20 21:47:42 +0700

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,65 +1,49 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import { auth } from "@/main.js";
+import { createRouter, createWebHistory } from 'vue-router'
+import { auth } from "@/firebase.js"
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'index',
-    component: () => import('../views/Index.vue')
-  },
-  {
-    path: '/404', // Первая, для перекрытия остальных
-    name: '404',
-    component: () => import('../views/HotFound.vue')
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    component: () => import('../views/Admin.vue'),
-    // children: [
-    //   {
-    //     path: '/admin/:razdel/page/:id?',
-    //     component: () => import('../compadmin/FormPage.vue'),
-    //   },
-    //   {
-    //     path: '/admin/:razdel/:id?',
-    //     component: () => import('../compadmin/FormRazdel.vue')
-    //   }
-    // ],
-    // ВКЛЮЧИТЬ на продакшине!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    meta: {
-      requiresAuth: true
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: () => import('../views/Index.vue')
+    },
+    {
+      path: '/404', // Первая, для перекрытия остальных
+      name: '404',
+      component: () => import('../views/HotFound.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/Admin.vue'),
+      // ВКЛЮЧИТЬ на продакшине!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/:razdel',
+      name: 'razdel',
+      component: () => import('../views/Razdel.vue')
+    },
+    {
+      path: '/:razdel/:page',
+      name: 'page',
+      component: () => import('../views/Page.vue')
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'notfound',
+      redirect: '/404'
     }
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/:razdel',
-    name: 'razdel',
-    component: () => import('../views/Razdel.vue')
-  },
-  {
-    path: '/:razdel/:page',
-    name: 'page',
-    component: () => import('../views/Page.vue')
-  },
-  {
-    path: '*',
-    name: 'notfound',
-    redirect: '/404'
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  ]
 })
 
 router.beforeEach((to, from, next) => {
